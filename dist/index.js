b25fd297e4d4405d745a4547f7c294949f04f740
Godswill Udida <udidagodswill7@gmail.com>
2023-10-13 04:20:11 +0100

    redeeming time

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mainApp_1 = require("./mainApp");
-const port = 1234;
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const port = parseInt(process.env.PORT);
 const app = (0, express_1.default)();
 (0, mainApp_1.mainApp)(app);
 const Server = app.listen(port, () => {
